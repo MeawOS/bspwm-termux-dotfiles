b216f0b09b73567ec17fcccc7c66eb0701d58183
BHUTUU <91358234+BHUTUUU@users.noreply.github.com>
2023-01-09 05:03:31 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,13 +5,15 @@ apt install x11-repo
 apt install git
 apt install bspwm
 apt install sxhkd
-
-git clone https://github.com/adi1090x/polybar-themes
+apt install tigervnc
+rm -rf ~/.vnc/xstartup
 mkdir ~/.uwu
 mv 12.png ~/.uwu
 mkdir ~/.config/
 mv bspwm ~/.config/
 mv sxhkd ~/.config
+git clone https://github.com/adi1090x/polybar-themes
+
 
 clear
 echo "ejecuta start.sh"
