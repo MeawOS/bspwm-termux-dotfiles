4a51b5804f4b728f6ab9c51e31af0db78bcbf533
BHUTUU <91358234+BHUTUUU@users.noreply.github.com>
2023-01-09 04:57:50 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,9 +5,9 @@ apt install x11-repo
 apt install git
 apt install bspwm
 apt install sxhkd
-mkdir ~/.polybar-themes
+
 git clone https://github.com/adi1090x/polybar-themes
-mv polybar-themes ~/.polybar-themes
+
 mkdir ~/.config/
 mv bspwm ~/.config/
 mv sxhkd ~/.config
