ed4b437683790b812bb0a21546a9fdbb1bb1f310
BHUTUU <91358234+BHUTUUU@users.noreply.github.com>
2023-01-09 05:05:01 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,10 +2,12 @@ pkg upgrade
 pkg update
 apt install root-repo
 apt install x11-repo
+apt install xcompmgr
 apt install git
 apt install bspwm
 apt install sxhkd
 apt install tigervnc
+apt install xfce4-terminal
 rm -rf ~/.vnc/xstartup
 mkdir ~/.uwu
 mv 12.png ~/.uwu
