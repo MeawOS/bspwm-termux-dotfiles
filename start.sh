2bf1b92f9de98e2d2cee222354ba0727d0cd9918
username12 <91358234+MeawOS@users.noreply.github.com>
2023-05-12 17:06:15 -0600

    Actualizar start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,29 @@
-vncserver -kill localhost:1 ; vncserver -geometry 1530x720
+#add repo
+
+echo - Topenrc-eudev] \nSigLevel Package Optional inserver=http://downloads.sourceforge.net/project/archopenrc/\$repo/\$arch" >> /etc/pacman.conf
+
+#remove systemd
+
+pacman Rdd systemd libsystemd
+
+#install openre
+
+pacman -Sy sysvinit openrc-core eudev eudev-openrc eudev-systemdcompat dbus-nosystemd procps-ng-nosystemd displaymanager-openrc
+
+#basic configuration
+
+nano /etc/rc.conf; nano /etc/conf.d/xdm; rc-update add xdm default
+
+rm -f /etc/hostname; echo "hostname=openrc" > /etc/conf.d/hostname
+
+# reboot
+
+sysctl kernel.sysrq=1
+
+echo "If above pacman operations reported no error" echo then sync - remount-ro reboot:"
+
+echo echo s> /proc/sysrq-trigger®
+
+echo echo u> /proc/sysrq-trigger
+
+echo echo b > /proc/sysrq-trigger
